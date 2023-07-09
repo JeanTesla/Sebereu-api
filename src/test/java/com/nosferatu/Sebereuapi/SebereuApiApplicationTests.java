0bec62345cdd062a393d70eb80ea730da1e6e8ce
JeanTesla <jean.tesla@outlook.com>
2023-07-09 20:12:40 -0300

    Initial Commit

---
+++ src/test/java/com/nosferatu/Sebereuapi/SebereuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosferatu.Sebereuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebereuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
