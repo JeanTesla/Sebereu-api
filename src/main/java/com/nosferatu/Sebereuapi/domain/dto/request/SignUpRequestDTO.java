724f62bf97207a3253e89a72ff7c4fb6d7e34bae
JeanTesla <jean.tesla@outlook.com>
2023-08-19 17:30:42 -0300

    Ciração de docker-compose e de profiles local e prod

--- src/main/java/com/nosferatu/Sebereuapi/domain/dto/request/SignUpRequestDTO.java
+++ src/main/java/com/nosferatu/Sebereuapi/domain/dto/request/SignUpRequestDTO.java
@@ -9,11 +9,16 @@ import lombok.Getter;
 public class SignUpRequestDTO {
 
     private String email;
+
+    private String name;
+
     private String password;
+
     private String passwordRepeat;
 
     public User toEntity(){
         return User.builder()
+                .name(this.name)
                 .email(this.email)
                 .password(this.password)
                 .build();
